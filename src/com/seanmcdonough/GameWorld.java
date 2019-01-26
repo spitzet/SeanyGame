64b359ed172721f2235997f10eb4c3521e6b596c
Travis <travissp87@gmail.com>
2019-01-25 19:16:45 -0800

    Werk

--- src/com/seanmcdonough/GameWorld.java
+++ src/com/seanmcdonough/GameWorld.java
@@ -2,30 +2,36 @@ package com.seanmcdonough;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class GameWorld {
 
-    ArrayList<FieldSquare> fieldSquares;
-    ArrayList<GameObject> gameObjects;
+    List<FieldSquare> fieldSquares;
+    List<GameObject> gameObjects;
     private int level;
     private int squaresOwned;
     Car car;
     int lives;
     private int timer;
     private int score;
+    private Random rand;
 
 
 
     public GameWorld() {
         gameObjects = new ArrayList<GameObject>();
         squaresOwned = 396;
+        level = 1;
         lives = 3;
-        timer = 20;
+        timer = 10;
+        rand = new Random();
         makeFieldSquares();
         makeGameObjects();
 
     }
     private void makeFieldSquares(){
+        fieldSquares = new ArrayList<>();
          //all squares along the border are owned at start. 396 total
         for(int i=0;i<9604;i++){
             FieldSquare fs = new FieldSquare();
@@ -37,23 +43,27 @@ public class GameWorld {
         }
     }
     private void makeGameObjects(){
-
+        gameObjects = new ArrayList<>();
         car = new Car( 0, 0, 0);
     }
     public void steerNorth(){
         car.setHeading(0);
+        System.out.println("Heading set to: " + car.getHeading());
     }
 
     public void steerSouth(){
         car.setHeading(180);
+        System.out.println("Heading set to: " + car.getHeading());
     }
 
     public void steerEast(){
         car.setHeading(90);
+        System.out.println("Heading set to: " + car.getHeading());
     }
 
     public void steerWest(){
         car.setHeading(270);
+        System.out.println("Heading set to: " + car.getHeading());
     }
 
     public void increaseSpeed(){
@@ -61,15 +71,19 @@ public class GameWorld {
     }
 
     public void decreaseSpeed(){
-
+        car.setSpeed((car.getSpeed()-1));
     }
 
     public void addMonsterBall(){
-
+        var heading = (rand.nextInt(3))*90;
+        var speed = 1;
+        MonsterBall monsterBall = new MonsterBall(heading, speed);
+        gameObjects.add(monsterBall);
     }
 
     public void addTimeTicket(){
-
+        TimeTicket timeTicket = new TimeTicket(level);
+        gameObjects.add(timeTicket);
     }
 
     public void grouped(){
@@ -82,8 +96,6 @@ public class GameWorld {
         MonsterBall ball = new MonsterBall(0, 10);
         Color newColor = new Color(1, 1, 1);
         ball.setColor(newColor);
-
-
     }
 
     public void completedSquare(){
--- src/com/seanmcdonough/TimeTicket.java
+++ src/com/seanmcdonough/TimeTicket.java
@@ -6,14 +6,16 @@ public class TimeTicket extends Fixed {
     public static final int WIDTH = 0;
     public static final int HEIGHT = 0;
     public int time;
+    private final int INITIAL_VALUE = 5;
+    private final int DECREMENT = 2;
 
-    public TimeTicket(){
-
+    public TimeTicket(int level){
+        time = INITIAL_VALUE - (DECREMENT * (level-1));
     }
 
 
     @Override
     public void setColor(Color color) {
-        //unchangable color
+        //unchangeable color
     }
 }
